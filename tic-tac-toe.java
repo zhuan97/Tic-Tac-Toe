edf1fff89401399a22f86605071d16a7d73026f5
Freneme <zhuan1997@gmail.com>
2020-08-05 22:03:50 -0700

    Update tic-tac-toe.java
    Reconstruct the code to improve reusability and readability.

--- tic-tac-toe.java
+++ tic-tac-toe.java
@@ -1,85 +1,37 @@
 import java.util.Scanner;
 
 class tictactoe {
-
     static Scanner input = new Scanner(System.in);
     static int n = 3;
     public static void main(String[] args){
         Scanner keyboard = new Scanner(System.in);
 
-        
         char[][] board = new char[n][n];
-        int player1Row = 0, player1Col = 0, player2Row = 0, player2Col = 0;
-        int[][] record = new int[9][2];
+        int[][] record = new int[n*n][2];
         int move = 0;
         boolean unfinished = true;
         System.out.println("Game Started, Player 1 moves first");
         while(unfinished){
-            boolean player1Move = true, player2Move = true;
-            while(player1Move){
-                System.out.println("Player 1: Which row you choose?");
-                player1Row = input.nextInt()-1;
-                System.out.println("Player 1: Which col you choose?");
-                player1Col = input.nextInt()-1;
-                if(player1Row>2 ||player1Row<0 ||player1Col>2||player1Col<0||board[player1Row][player1Col] != (char) 0){
-                    System.out.println("Player 1: Unavailable placement, please place another spot.");
-                }else{
-                    record [move][0] = player1Row;
-                    record [move][1] = player1Col;
-                    move++;
-                    board[player1Row][player1Col] = 'X';
-                    player1Move = false;
-                }
-                System.out.println("Current Board");
-                for(int i = 0;i <n;i++){
-                    for(int j = 0; j<n;j++){
-                        if(j == n-1){
-                            System.out.print("|" + board[i][j] +"|");
-                        }else{
-                            System.out.print("|" + board[i][j] );
-                        }
-                        
-                    }
-                    System.out.println();
-                }
-            }
-            if(checkWin(board,player1Row,player1Col,'X')) {
+            // Player 1 Move
+            playerMove(board,record, 'X', move,1);
+            move++;
+            // Check player 1 win condition
+            if(checkWin(board,record[move-1][0],record[move-1][1],'X')) {
                 System.out.println("Player 1 win!");
                 unfinished = false;
                 break;
             }
-
-            if(record[8][0]!= (char) 0){
+            // Check draw condition
+            if(record[(n*n)-1][0]!= (char) 0){
                 System.out.println("Draw!");
                 unfinished = false;
                 break;
             }
-            while(player2Move){
-                System.out.println("Player 2: Which row you choose?");
-                player2Row = input.nextInt()-1;
-                System.out.println("Player 2: Which col you choose?");
-                player2Col = input.nextInt()-1;
-                if(player2Row>2 ||player2Row<0 ||player2Col>2||player2Col<0||board[player2Row][player2Col] != (char) 0){
-                    System.out.println("Player 2: Unavailable placement, please place another spot.");
-                }else{
-                    record [move][0] = player2Row;
-                    record [move][1] = player2Col;
-                    move++;
-                    board[player2Row][player2Col] = 'O';
-                    player2Move = false;
-                }
-                for(int i = 0;i <n;i++){
-                    for(int j = 0; j<n;j++){
-                        if(j == n-1){
-                            System.out.print("|" + board[i][j] +"|");
-                        }else{
-                            System.out.print("|" + board[i][j] );
-                        }
-                    }
-                    System.out.println();
-                }
-            }
-            if(checkWin(board,player2Row,player2Col,'O')) {
+            // Player 2 Move
+            playerMove(board,record, 'O', move,2);
+            move++;
+            // Check player 2 win condition
+            if(checkWin(board,record[move-1][0],record[move-1][1],'O')) {
                 System.out.println("Player 2 win!");
                 unfinished = false;
                 break;
@@ -87,24 +39,60 @@ class tictactoe {
         }	
         keyboard.close();
 }
+    // Player move function
+    private static void playerMove(char[][] board, int[][] record, char playerChar,int move, int playerNum){
+        boolean playerMove = true;
+        while(playerMove){
+            int playerRow = 0, playerCol = 0; 
+            System.out.println("Player " + playerNum +" : Which row you choose?");
+            playerRow = input.nextInt()-1;
+            System.out.println("Player " + playerNum +" : Which col you choose?");
+            playerCol = input.nextInt()-1;
+            if(playerRow>n-1 ||playerRow<0 ||playerCol>n-1||playerCol<0||board[playerRow][playerCol] != (char) 0){
+                System.out.println("Player " + playerNum +" : Unavailable placement, please place another spot.");
+            }else{
+                record [move][0] = playerRow;
+                record [move][1] = playerCol;
+                board[playerRow][playerCol] = playerChar;
+                playerMove = false;
+            }
+        displayBoard(board);
+        }
+    }
 
+    // Game Board display function
+    private static void displayBoard(char[][] board){
+        System.out.println("Current Board");
+        for(int i = 0;i <n;i++){
+            for(int j = 0; j<n;j++){
+                if(j == n-1){
+                    System.out.print("|" + board[i][j] +"|");
+                }else{
+                    System.out.print("|" + board[i][j] );
+                }
+                
+            }
+            System.out.println();
+        }
+    }
 
+    // Win condition check function
     private static boolean checkWin(char[][] board, int row, int col, char player) {
         boolean win = true;
-        //check row
+        // Check row
         for(int i = 0;i<n;i++){
             if(board[i][col] != player) win = false;
         }
         if(win) return true;
         
-        //check col 
+        // Check col 
         win = true;
         for(int i = 0;i<n;i++){
             if(board[row][i] != player) win = false;
         }
         if(win) return true;
         
-        //check diagonal
+        // Check diagonal
         if(row == col){
             win = true;
             for(int i = 0;i<n;i++){
@@ -112,7 +100,7 @@ class tictactoe {
             }
             if(win) return true;
         }
-        //check the other diagonal
+        // Check the other diagonal
         if(row == n - col -1){
             win = true;
             for(int i = 0;i<n;i++){
